4675dd983ecfa602012af04f83e839989868f62d
roshan <roshanfreelancer499@gmail.com>
2026-02-13 22:43:48 +0530

    express setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("project started");
+import express from 'express';
+
+const app = express();
+
+app.get('', (req, res, next) => {
+    res.send('Hello Express');
+});
+
+app.listen(8000, () => {
+    console.log("Application is running on port localhost:8000");
+});
