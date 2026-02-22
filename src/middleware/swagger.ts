9afe735473da5bf0aabbb40bd8a87d422703cd84
roshan <roshanfreelancer499@gmail.com>
2026-02-22 09:52:53 +0530

    swagger-setup

---
+++ src/middleware/swagger.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Application } from "express";
+import swaggerUi from "swagger-ui-express";
+
+export default class SwaggerMiddlewareConfig {
+    public static setUp(expressApp: Application):void{
+        expressApp.use(
+            "/api-doc",
+            swaggerUi.serve,
+            swaggerUi.setup(undefined, {
+                swaggerOptions: {
+                    url: "/swagger.json",
+                }
+            })
+        );
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
-import express from 'express';
+import express from "express";
+import path from "path";
+import SwaggerMiddlewareConfig from "./middleware/swagger.ts";
 
 const app = express();
 
-app.get('', (req, res, next) => {
-    res.send('Hello Express');
+app.use(express.json());
+
+/* Serve swagger.json */
+app.get("/swagger.json", (req, res) => {
+  res.sendFile(path.resolve(process.cwd(), "public/swagger.json"));
+});
+
+/* Setup swagger BEFORE routes */
+SwaggerMiddlewareConfig.setUp(app);
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
 });
 
 app.listen(8000, () => {
-    console.log("Application is running on port localhost:8000");
+  console.log("Server running at http://localhost:8000");
 });
